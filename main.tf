e53417b8aae86d97302acc56213c6ce09391e901
M M <deuxemm@vivaldi.net>
2022-10-21 08:54:38 +0100

    added dev-subnet-2 into default VPC

--- main.tf
+++ main.tf
@@ -12,5 +12,16 @@ resource "aws_subnet" "dev-subnet-1" {
     availability_zone = "us-east-1a"
 }
 
+data "aws_vpc" "existing_vpc" {
+    default = true
+}
+
+resource "aws_subnet" "dev-subnet-2" {
+    vpc_id = data.aws_vpc.existing_vpc.id
+    cidr_block = "172.31.96.0/20"
+    availability_zone = "us-east-1a"
+}
+
+
 
 
