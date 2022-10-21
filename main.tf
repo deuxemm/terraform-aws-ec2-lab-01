6e813b7dfad862581f61702e1b3de36acf68cc1c
M M <deuxemm@vivaldi.net>
2022-10-21 09:53:57 +0100

    added Name tags

--- main.tf
+++ main.tf
@@ -4,12 +4,18 @@
 
 resource "aws_vpc" "development-vpc" {
     cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "dev-priv"
+    }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
     vpc_id = aws_vpc.development-vpc.id
     cidr_block = "10.0.10.0/24"
     availability_zone = "us-east-1a"
+    tags = {
+        Name = "dev-subnet-1-priv"
+    }
 }
 
 data "aws_vpc" "existing_vpc" {
@@ -20,6 +26,9 @@ resource "aws_subnet" "dev-subnet-2" {
     vpc_id = data.aws_vpc.existing_vpc.id
     cidr_block = "172.31.96.0/20"
     availability_zone = "us-east-1a"
+     tags = {
+        Name = "dev-subnet-2-default"
+    }
 }
 
 
